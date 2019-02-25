b369d93176c955129992e14a80b037cd7597666b
Sonia Khatri <sonia_khatri@persistent.co.in>
2019-02-25 16:01:56 +0530

    Post request to simulator for generating scanning api data

--- Middleware/jobs/AccessPointClientsJob.js
+++ Middleware/jobs/AccessPointClientsJob.js
@@ -1,60 +1,77 @@
 var schedule = require("node-schedule");
 var config = require("config");
 var rn = require('random-number');
-const http = require("http");
-var promise = require("bluebird");
+var Request = require("request");
+const debug = require("debug");
 
 
-  var job = function AccessPointClientsJob() {
-	schedule.scheduleJob(config.get("environment.constants.apclientsJobTimer"), function () {
-		var datetime = new Date();
+var job = function AccessPointClientsJob() {
+    schedule.scheduleJob(config.get("environment.constants.apclientsJobTimer"), function () {
+        var datetime = new Date();
         console.log("Sample Job Running at : ", datetime);
 
-        var apList = [];
-        apList.push(config.get("simulator.scanning.apMac1"));
-        apList.push(config.get("simulator.scanning.apMac2"));
-        apList.push(config.get("simulator.scanning.apMac3"));
-        apList.push(config.get("simulator.scanning.apMac4"));
-
-        console.log('Printing aplist ',apList);
-        var gen = rn.generator({
-            min:  2,
-            max:  10,
-            integer: true
-          })
-        var apDeviceCountMap = new Map();
-        for(var apMacAddr in apList){
-            apDeviceCountMap.set(apMacAddr,  gen())
-        }
-        console.log('Printing apdevicecountmap ... ',apDeviceCountMap); 
         //POST call to scanning api method to perform logic and db insertion.
         _performUrlPost().then(function (result) {
-            console.log('simulated apclient data is stored in database.')
-		});
+            console.log('simulated apclient data is stored in database.');
+            console.log('Printing promise result ',result);
+        });
     });
 };
 
 function _performUrlPost() {
+
+    var apList = [];
+    apList.push(config.get("simulator.scanning.apMac1"));
+    apList.push(config.get("simulator.scanning.apMac2"));
+    apList.push(config.get("simulator.scanning.apMac3"));
+    apList.push(config.get("simulator.scanning.apMac4"));
+
+    console.log('Printing aplist ', apList);
+    var gen = rn.generator({
+        min: 2,
+        max: 10,
+        integer: true
+    })
+
+    var reqPostParams = {};
+    let arrayOfData = [];
+    for (var apMacAddr in apList) {
+        let apClientsObject = {};
+        apClientsObject.apMacAddr = apMacAddr;
+        apClientsObject.numberOfClients = gen();
+        arrayOfData.push(apClientsObject);
+
+    }
+    reqPostParams.data = arrayOfData;
+
     var url = config.get("simulator.scanning.apiForSimulatedScanningApiData");
     return new Promise(function (fulfill, reject) {
-        http.get(url, (resp) => {
-            let data = "";
-            console.log("response status ", resp.statusCode);
-            console.log("response message ", resp.statusMessage);
-            resp.on("data", (chunk) => {
-                data += chunk;
-            });
-            // The whole response has been received. Print out the result.
 
-            resp.on("end", () => {
-                fulfill(JSON.parse(data));
-            });
+        Request.post({
+            "headers": {
+                "Content-Type": "application/json",
+                "Content-Length": JSON.stringify(reqPostParams).length
+            },
+            "url": config.get("simulator.scanning.apiForSimulatedScanningApiData"),
+            "body": JSON.stringify(reqPostParams)
+        }, (error, response, body) => {
+            if (error) {
+                debug("Error: " + error.message);
+                reject(error);
 
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-            reject(err);
+            }
+            else {
+                let returnData = {};
+                returnData = JSON.parse(body);
+                debug("Response success : " + returnData);
+                fulfill(returnData);
+            }
         });
     });
 }
 
 module.exports.clientsJob = job;
+
+
+
+
--- Middleware/routes/scanningApi/index.js
+++ Middleware/routes/scanningApi/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res) {
+router.post("/", function (req, res) {
     console.log('Scanning api route is working ');
     let response = {};
     response.message = "SCANNING API ROUTE IS WORKING";
