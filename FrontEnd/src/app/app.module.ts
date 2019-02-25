2deb72609937b016103bea9c19b093e3ee1b187e
Subhadip Jot <subhadip_jot@persistent.co.in>
2019-02-25 18:32:47 +0530

    Cards and Charts Updated with Dummy Data

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,15 +11,24 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { InformationCardsComponent } from './components/information-cards/information-cards.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { StackedBarChartComponent } from './components/stacked-bar-chart/stacked-bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavComponent
+    TopNavComponent,
+    SideNavComponent,
+    InformationCardsComponent,
+    ChartsComponent,
+    StackedBarChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ChartsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule
