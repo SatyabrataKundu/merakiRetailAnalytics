6062abcb2eb52afeaff7be91ab875aa68ec08385
harshal_patil <harshal_patil@persistent.co.in>
2019-02-26 10:16:58 +0530

    added loop and method changes

--- Middleware/jobs/PosDataGenerationJob.js
+++ Middleware/jobs/PosDataGenerationJob.js
@@ -2,16 +2,15 @@
 var shedule = require('node-schedule');
 /* run the job at every 5 mins*/
 var event = shedule.scheduleJob('*/5 * * * *', function () {
-    var randomNumberOfItems = getRandomInt(10);
-    var randomTotalAmount = getRandomInt(10000);
+    for(var i = 0; i < getRandomInt(10); i++) {
+        var randomNumberOfItems = getRandomInt(10);
+        var randomTotalAmount = getRandomInt(10000);
 
-    console.log('This runs every 5 minutes' + randomNumberOfItems +' ' +randomTotalAmount);
+         console.log('This runs every 5 minutes' + randomNumberOfItems +' ' +randomTotalAmount);
+       }
+    
 });
 
-function getRandomNumberOfItems(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-function getRandomTotalAmount(max) {
+function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
