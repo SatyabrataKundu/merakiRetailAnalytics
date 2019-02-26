6a0cc6da4c0b706db82bf431338000e2ea5073d7
harshal_patil <harshal_patil@persistent.co.in>
2019-02-26 15:06:37 +0530

    Added table for pos data

--- Middleware/database/init.sql
+++ Middleware/database/init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE merakidb;
 CREATE SCHEMA meraki;
 
 CREATE SEQUENCE meraki.scanning_ap_data_unique_scanning_key_seq;
+CREATE SEQUENCE meraki.pos_data_key_seq;
 
 
 CREATE TABLE meraki.scanning_ap_data
@@ -22,10 +23,32 @@ CREATE TABLE meraki.scanning_ap_data
 	seen_epoch character varying COLLATE pg_catalog."default",
     CONSTRAINT scanning_ap_data_pkey PRIMARY KEY (unique_scanning_key)
 )
+
+CREATE TABLE meraki.pos_data
+(
+    unique_pos_data_key integer NOT NULL DEFAULT nextval('meraki.pos_data_key_seq'::regclass),
+    datetime character varying COLLATE pg_catalog."default",
+    dateformat_date character varying(12) COLLATE pg_catalog."default",
+	dateformat_year integer DEFAULT 2019,
+    dateformat_month integer,
+    dateformat_week integer,
+    dateformat_day integer,
+    dateformat_hour integer,
+    dateformat_minute integer,
+	uuid integer,
+    no_of_items integer,
+    total_amount integer,
+    pos_counter_number integer,
+    CONSTRAINT pos_data_pkey PRIMARY KEY (unique_pos_data_key)
+)
+
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
 ALTER TABLE meraki.scanning_ap_data
+    OWNER to postgres;
+
+ALTER TABLE meraki.pos_data
     OWNER to postgres;
