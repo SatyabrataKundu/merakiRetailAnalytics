3238620c7d6c4785c72aa97df0bf7b1ee2e321c5
SatyabrataKundu <satyabrata_kundu@persistent.co.in>
2019-02-26 16:04:24 +0530

    exponse test endpoint for random visitor count

--- Middleware/routes/scanningApi/index.js
+++ Middleware/routes/scanningApi/index.js
@@ -1,10 +1,17 @@
 var express = require("express");
 var router = express.Router();
+var rn = require('random-number');
 
 router.get("/", function (req, res) {
     console.log('Scanning api route is working ');
     let response = {};
+    var gen = rn.generator({
+        min: 200,
+        max: 10000,
+        integer: true
+    })
     response.message = "SCANNING API ROUTE IS WORKING";
+    response.count = gen();
     res.send(response);
 });
 
