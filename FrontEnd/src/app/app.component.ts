f8eccdfae84bc2165cc08d977ba08d7939d6f610
Subhadip Jot <subhadip_jot@persistent.co.in>
2019-03-05 16:54:33 +0530

    Wokring POS wait time data

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'retailanalytics';
+  posWaitTime:any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get('http://localhost:4004/api/v0/meraki/checkout/waitTime')
+    .subscribe(res => {
+      this.posWaitTime = res;
+    })
+  }
 }
+
+
