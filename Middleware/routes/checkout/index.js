ae7b13b2053bff28625519505e5e08ad5dd85207
Sonia Khatri <sonia_khatri@persistent.co.in>
2019-03-06 16:04:23 +0530

    changed jobs to run every 10 minutes interval

--- Middleware/routes/checkout/index.js
+++ Middleware/routes/checkout/index.js
@@ -18,12 +18,12 @@ router.get("/waitTime", function (req, res) {
     // res.status(200).send("success");
     var endDate = new Date();
     var startdate = new Date();
-    var durationInMinutes = 10;
+    var durationInMinutes = config.get("simulator.checkout.queueconstant");
     startdate.setMinutes(endDate.getMinutes() - durationInMinutes);
     console.log("Start Date " + startdate);
     console.log("End Date " + endDate);
 
-    let query = "select (case when ROUND((count(distinct (cam.person_oid)) - count(distinct(unique_pos_data_key)))/2.0,2)>0 Then  ROUND((count(distinct (cam.person_oid)) - count(distinct(unique_pos_data_key)))/2.0,2) ELSE 0 END) as waitTime, pos.pos_counter_number as posId from meraki.camera_detections cam right outer join meraki.checkoutzone_billingcounter_map mapp on cam.zoneid=mapp.zone_id right outer join meraki.pos_data pos on mapp.pos_counter_number=pos.pos_counter_number where "
+    let query = "select (case when ROUND((count(distinct (cam.person_oid)) - count(distinct(unique_pos_data_key)))/10.0,2)>0 Then  ROUND((count(distinct (cam.person_oid)) - count(distinct(unique_pos_data_key)))/10.0,2) ELSE 0 END) as waitTime, pos.pos_counter_number as posId from meraki.camera_detections cam right outer join meraki.checkoutzone_billingcounter_map mapp on cam.zoneid=mapp.zone_id right outer join meraki.pos_data pos on mapp.pos_counter_number=pos.pos_counter_number where "
         + "(cam.datetime between " + startdate.getTime() + " and " + endDate.getTime() + ") "
         + "and (pos.datetime between  " + startdate.getTime() + " and " + endDate.getTime() + ") "
         + "group by cam.zoneid,pos.pos_counter_number";
