b246a1af7e2bc99051e1f186cfe873a11ae4fce7
Sonia Khatri <sonia_khatri@persistent.co.in>
2019-03-09 18:10:47 +0530

    updating db tables for real data

--- Middleware/database/init8.sql
+++ Middleware/database/init8.sql
@@ -1,6 +1,6 @@
 
 truncate table meraki.realtime_zones;
 
-insert into meraki.realtime_zones (zone_id, zone_name) values (668784544664518754, 'Checkout');
-insert into meraki.realtime_zones (zone_id, zone_name) values (668784544664518755, 'Entrance');
-insert into meraki.realtime_zones (zone_id, zone_name) values (668784544664518756, 'Exit');
+insert into meraki.realtime_zones (zone_id, zone_name) values (668784544664518757, 'Entrance_Zone');
+insert into meraki.realtime_zones (zone_id, zone_name) values (668784544664518758, 'Billing_Zone');
+insert into meraki.realtime_zones (zone_id, zone_name) values (668784544664518759, 'Exit_Zone');
