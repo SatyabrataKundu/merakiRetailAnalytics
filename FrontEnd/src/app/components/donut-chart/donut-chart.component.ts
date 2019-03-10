9cca4cfc0e37b1365b43685d0f78160ae38435e9
Vidya <vidprabh@cisco.com>
2019-03-09 19:36:58 -0800

    amandonment changes

--- FrontEnd/src/app/components/donut-chart/donut-chart.component.ts
+++ FrontEnd/src/app/components/donut-chart/donut-chart.component.ts
@@ -12,7 +12,7 @@ export class DonutChartComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
   public chartType: string = "doughnut";
-  public chartLabels: Array<string> = ['Total Transactions', 'Total Visitors',  'Total Checkouts'];
+  public chartLabels: Array<string> = ['Total Transactions', 'Total Visitors',  'Total Checkouts',  'Total Abandonments'];
   public chartLabels2: Array<string> = [];
   public chartData: Array<string> = [];
   public chartData2: Array<number> = [350, 150, 100, 45, 23, 12, 16, 70];
@@ -97,6 +97,7 @@ export class DonutChartComponent implements OnInit {
     let totalTransactions = "0";
     let totalVisitors = "0";
     let totalCheckouts = "0";
+    let totalAbandonments = "0";
 
     Observable
     timer(1, 1000 * 30).subscribe(() =>
@@ -106,6 +107,9 @@ export class DonutChartComponent implements OnInit {
           this.http.get('http://localhost:4004/api/v0/meraki/scanning/visitorCountByDate')
             .subscribe(res => {
               totalVisitors = res[0].count;
+              this.http.get('http://localhost:4004/api/v0/meraki/checkout/totalCheckoutZoneAbandonmentsToday')
+             .subscribe(res => {
+              totalAbandonments = res[0].count;
               this.http.get('http://localhost:4004/api/v0/meraki/checkout/totalCheckoutZoneVisitorsToday')
                 .subscribe(res => {
                   this.chartData = [];
@@ -113,10 +117,11 @@ export class DonutChartComponent implements OnInit {
                   this.chartData.push(totalTransactions);
                   this.chartData.push(totalVisitors);
                   this.chartData.push(totalCheckouts);
+                  this.chartData.push(totalAbandonments);
                   this.salesDonutChartUpdate(this.chartData);
                 })
-            })
-
+              })
+            })      
         })
     )
 
